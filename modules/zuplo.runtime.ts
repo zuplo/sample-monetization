27e37f1f6922e453f30ccd56fb22caaeae49f7a4
lcampos <luis@zuplo.com>
2024-02-01 17:31:59 +0000

    Initial commit from Zuplo connect

---
+++ modules/zuplo.runtime.ts
@@ -0,0 +1,16 @@
+import {
+  RuntimeExtensions,
+  StripeMeteringPlugin,
+  environment,
+} from "@zuplo/runtime";
+
+export function runtimeInit(runtime: RuntimeExtensions) {
+  const stripe = new StripeMeteringPlugin({
+    webhooks: {
+      signingSecret: environment.STRIPE_WEBHOOK_SIGNING_SECRET,
+    },
+    zuploAccountApiKey: "somethin",
+    stripeSecretKey: environment.STRIPE_SECRET_KEY,
+  });
+  runtime.addPlugin(stripe);
+}
